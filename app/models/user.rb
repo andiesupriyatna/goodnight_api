0e755c55506afcda93bb63cdb880d530ad37be75
Andie Hariyadi Supriyatna <hariyadi.andie@gmail.com>
2025-04-18 10:54:33 +0700

    add followersuser relation to user relation  https://trello.com/c/5yBUMomv

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,7 @@ class User
 
   field :name, type: String
   has_many :sleep_history, dependent: :destroy
+  has_many :following, class_name: 'Core::FollowerUser'
+  has_many :followers, class_name: 'Core::FollowerUser'
 
 end
