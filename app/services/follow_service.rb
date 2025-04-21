d63656a2fcb2c01ea29d5f62bf10f1a9ccec7a71
Andie Hariyadi Supriyatna <hariyadi.andie@gmail.com>
2025-04-21 23:09:04 +0700

    develop logic for follow user https://trello.com/c/s1BpgFzz

--- app/services/follow_service.rb
+++ app/services/follow_service.rb
@@ -1,6 +1,23 @@
 class FollowService
-    def follow_user()
-        
+    def follow_user(user:, followed_id:)
+      user_to_follow = User.find(followed_id)
+  
+      if user == user_to_follow
+        return { error: "You can't follow yourself" }, :unprocessable_entity
+      end
+  
+      existing_follow = FollowerUser.where(follower: user, followed: user_to_follow).first
+      if existing_follow
+        return { error: "Already following this user" }, :unprocessable_entity
+      end
+  
+      follow = FollowerUser.new(follower: user, followed: user_to_follow)
+  
+      if follow.save
+        return { message: "Followed successfully", follow: follow }, :ok
+      else
+        return { error: follow.errors.full_messages }, :unprocessable_entity
+      end
     end
 
     def unfollow_user()
