267bf1269d8df785e305fa7222009b34834e0ce4
Andie Hariyadi Supriyatna <hariyadi.andie@gmail.com>
2025-04-21 21:07:05 +0700

    Remove config rails with sqllite, add with mongoDB

--- README.md
+++ README.md
@@ -6,13 +6,13 @@ application up and running.
 Things you may want to cover:
 
 * Ruby version
-   3.2.2
+
 * System dependencies
-   Rails and MongoDB
+
 * Configuration
 
 * Database creation
-   MongoDB
+
 * Database initialization
 
 * How to run the test suite
@@ -22,10 +22,3 @@ Things you may want to cover:
 * Deployment instructions
 
 * ...
-
-
-
-*link*
-Document Good Night Api : https://docs.google.com/document/d/1lAScOHghlSRXzyJTPjoY-ydaEHOFDZaeDADFY62QVwI
-Project tracker : https://trello.com/b/n73vn50f/goodnight-api
-
--- app/models/sleep_history.rb
+++ app/models/sleep_history.rb
@@ -8,24 +8,6 @@ class SleepHistory
 
   belongs_to :user
 
-  # Validations
-  validates :clock_in_time, presence: true
-  validates :user, presence: true
-  validate :clock_out_time_after_clock_in_time, if: -> { clock_out_time.present? }
-
-  # Scopes
-  scope :completed, -> { where(:clock_out_time.ne => nil) }
-  scope :past_week, -> { where(:clock_in_time.gte => 1.week.ago) }
-  
-  # Indexes
-  index({ user_id: 1 })
-  index({ clock_in_time: 1 })
-  index({ sleep_duration: 1 })
-  index({ user_id: 1, clock_in_time: 1 })
-  
-  # Callbacks
-  before_save :calculate_sleep_duration, if: -> { clock_out_changed? && clock_out.present? }
-
   def clock_out!
     update(clock_out: Time.current) unless clock_out.present?
   end
@@ -36,5 +18,34 @@ class SleepHistory
     if clock_out <= clock_in
       errors.add(:clock_out, "must be after the clock in time")
     end
+  end
+
+  def calculate_sleep_duration
+    return "N/A" unless clock_in && clock_out
+
+    seconds = (clock_out - clock_in).to_i
+    years = seconds / 31_536_000
+    seconds %= 31_536_000
+    months = seconds / 2_629_746
+    seconds %= 2_629_746
+    weeks = seconds / 604_800
+    seconds %= 604_800
+    days = seconds / 86_400
+    seconds %= 86_400
+    hours = seconds / 3_600
+    seconds %= 3_600
+    minutes = seconds / 60
+    seconds %= 60
+
+    time_string = []
+    time_string << "#{years} years" if years > 0
+    time_string << "#{months} months" if months > 0
+    time_string << "#{weeks} weeks" if weeks > 0
+    time_string << "#{days} days" if days > 0
+    time_string << "#{hours} hours" if hours > 0
+    time_string << "#{minutes} minutes" if minutes > 0
+    time_string << "#{seconds} seconds" if seconds > 0
+
+    time_string.join(" ")
   end
 end
