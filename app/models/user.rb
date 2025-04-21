190fbb8ea620f9670e1e46abd0357ab9ce7f745c
Andie Hariyadi Supriyatna <hariyadi.andie@gmail.com>
2025-04-21 23:05:20 +0700

    update model user model https://trello.com/c/5yBUMomv

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,7 @@ class User
   include Mongoid::Timestamps
 
   field :name, type: String
-  has_many :sleep_history, dependent: :destroy
-  has_many :following, class_name: 'Core::FollowerUser'
-  has_many :followers, class_name: 'Core::FollowerUser'
-
+  has_many :sleep_history, class_name: 'SleepHistory', dependent: :destroy
+  has_many :following, class_name: 'FollowerUser'
+  has_many :followers, class_name: 'FollowerUser'
 end
