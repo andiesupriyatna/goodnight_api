0a304a19a0bdae1b26fd7554cf0ccfcefa6812ef
Andie Hariyadi Supriyatna <hariyadi.andie@gmail.com>
2025-04-21 23:04:43 +0700

    fix clockin clockout controller and sleep history service https://trello.com/c/bjTAaeB0

--- app/controllers/sleep_history_controller.rb
+++ app/controllers/sleep_history_controller.rb
@@ -1,13 +1,30 @@
 class SleepHistoryController < ApplicationController
+  before_action :find_user
+
+  def index
+    sleep_history_service = SleepHistoryService.new
+    response_json, status = sleep_history_service.get_list_history(@user)
+    render json: response_json,status: status
+  end
+  
   #URL /sleep_history/clock_in POST
   def clock_in
-    result = sleep_history_service.clock_in_user(user:current_user, clock_in_time: Time.current)
-    json_response(result, :created)
+    sleep_history_service = SleepHistoryService.new
+    response_json, status = sleep_history_service.clock_in_user(user:@user, clock_in_time: Time.current)
+    render json: response_json,status: status
   end
 
   #URL /sleep_history/clock_out POST
   def clock_out
-    result = sleep_history_service.clock_out_user(user:current_user)
-    json_response(result, :created)
+    sleep_history_service = SleepHistoryService.new
+    response_json, status = sleep_history_service.clock_out_user(@user)
+    render json: response_json,status: status
+  #  json_response(result, :created)
+  end
+
+
+  private
+  def find_user
+    @user = User.find(params[:user_id])
   end
 end
--- app/services/sleep_history_service.rb
+++ app/services/sleep_history_service.rb
@@ -1,24 +1,34 @@
 class SleepHistoryService
-    def clock_in_user(user:current_user, clock_in_time: Time.current)
+    def clock_in_user(user, clock_in_time: Time.current)
         sleep_history = SleepHistory.create!(user: user, clock_in: Time.now)
     
-        render json: {
+        return {
           message: "Clocked in successfully",
           sleep_history: sleep_history.as_json(methods: :duration)
-        }, status: :ok
+        }, :ok
     end
 
     def clock_out_user(user)
-        sleep_history = user.sleep_histories.where(clock_out: nil).last
+        Rails.logger.info(user.sleep_history)
+        sleep_history = user.sleep_history.where(clock_out: nil).last
     
         if sleep_history
           sleep_history.update(clock_out: Time.now)
-          render json: {
+          return {
             message: "Clocked out successfully",
             sleep_history: sleep_history.as_json(methods: :duration)
-          }, status: :ok
+          }, :ok
         else
-          render json: { error: "No active clock-in found" }, status: :unprocessable_entity
+          return { error: "No active clock-in found" }, :unprocessable_entity
         end
+    end
+
+    def get_list_history(user)
+      sleep_history = user.sleep_history.order_by(created_at: :desc)
+
+      return {
+          message: "List History",
+          sleep_history: sleep_history.as_json(methods: :duration)
+        }, :ok
     end
 end
