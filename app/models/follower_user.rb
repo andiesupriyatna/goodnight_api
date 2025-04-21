e2b86ec665ae73d13e4aab503b43d78434e7e13e
Andie Hariyadi Supriyatna <hariyadi.andie@gmail.com>
2025-04-21 23:09:36 +0700

    updated user model for to become inverse of  https://trello.com/c/LwI4hSYJ

--- app/models/follower_user.rb
+++ app/models/follower_user.rb
@@ -2,7 +2,6 @@ class FollowerUser
   include Mongoid::Document
   include Mongoid::Timestamps
 
-  belongs_to :follower, class_name: 'User'
-  belongs_to :followed, class_name: 'User'
-
+  belongs_to :follower, class_name: 'User', inverse_of: :follows_as_follower
+  belongs_to :followed, class_name: 'User', inverse_of: :follows_as_followed
 end
