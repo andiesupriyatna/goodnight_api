ef8564076a6c9f046ae4c2c71f22bed45ff62582
Andie Hariyadi Supriyatna <hariyadi.andie@gmail.com>
2025-04-22 12:05:01 +0700

    add indexing for follower user and sleep history

--- app/models/follower_user.rb
+++ app/models/follower_user.rb
@@ -4,4 +4,8 @@ class FollowerUser
 
   belongs_to :follower, class_name: 'User', inverse_of: :follows_as_follower
   belongs_to :followed, class_name: 'User', inverse_of: :follows_as_followed
+
+  index({ follower_id: 1, followed_id: 1 }, { unique: true })
+  index({ follower_id: 1 })
+  index({ followed_id: 1 })
 end
